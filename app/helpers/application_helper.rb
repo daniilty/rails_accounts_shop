f95f6667616152aaaebfbd342f24263b5ebd90f8
Daniel <adef96@mail.ru>
2020-10-01 10:34:04 +0300

    lime

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(samp_title = "")
-    base_title = "Sample shit"
+    base_title = "Account shop"
     if samp_title.empty?
       return base_title
     end
